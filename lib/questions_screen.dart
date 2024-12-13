5ed68d15e13da69a13cd26b8b27cffe3ba7df972
Lincxx <bossco28@gmail.com>
2024-12-13 12:57:49 -0500

    Passing Data visa function across widgets

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -4,7 +4,12 @@ import 'package:quiz_app/data/questions.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({
+    super.key,
+    required this.onSelectedAnswer,
+  });
+
+  final void Function(String answer) onSelectedAnswer;
 
   @override
   State<QuestionsScreen> createState() => _QuestionsScreenState();
@@ -13,8 +18,10 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answerQuestion() {
-    //currentQuestionIndex = currentQuestionIndex + 1;
+  void answerQuestion(String selectedAnswers) {
+    //this gives us access to the StatefulWidget - widget
+    widget.onSelectedAnswer(selectedAnswers);
+    //currentQuestionIndex = currentQuestionIndex + 1;ƒ
     //currentQuestionIndex += 1;
     setState(() {
       currentQuestionIndex++;
@@ -49,7 +56,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             ...currentQuestion.getShuffledAnswer().map((answer) {
               return AnswerButton(
                 answerText: answer,
-                onTap: answerQuestion,
+                onTap: () {
+                  answerQuestion(answer);
+                },
               );
             }),
           ],
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -10,12 +10,17 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  final List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
       activeScreen = 'questions-screen';
     });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
   }
 
   @override
@@ -35,7 +40,7 @@ class _QuizState extends State<Quiz> {
           ),
           child: activeScreen == 'start-screen'
               ? StartSceen(switchScreen)
-              : const QuestionsScreen(),
+              : QuestionsScreen(onSelectedAnswer: chooseAnswer,),
         ),
       ),
     );
